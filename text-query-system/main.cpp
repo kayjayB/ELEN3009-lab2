177cbe4775e87a6ed8fc49e68f29bdd60afd908f
Kayla-Jade Butkow <kaylabutkow@gmail.com>
2017-08-17 17:25:28 +0200

    Exercise 5.4

---
+++ text-query-system/main.cpp
@@ -0,0 +1,53 @@
+#include "Word.h"
+#include "Line.h"
+#include "Paragraph.h"
+#include "FileReader.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+string file_name;
+string word_to_search;
+
+cout << "Please enter a file name:  " ;
+cin >> file_name;
+
+while (true)
+{
+	cout << endl;
+
+	cout << "Please enter a word to search for or \".\" to quit:  " ;
+	cin >> word_to_search;
+
+	if (word_to_search == ".")
+		break;
+
+	FileReader read_file(file_name);
+
+	Paragraph search_text;
+	read_file.readFileInto(search_text);
+
+	vector<int> line_numbers;
+	bool check =search_text.contains(word_to_search, line_numbers);
+
+	if (check)
+	{
+		cout << "Word found: " << endl;
+		for (int i=0; i<line_numbers.size();i++)
+		{
+			cout << "line " << line_numbers[i] << endl;
+		}
+	}
+	else 
+	{
+		cout << "Word not found" << endl;
+	}
+}
+
+	return 0;
+}
+
+
+		
